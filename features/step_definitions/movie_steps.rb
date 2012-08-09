e5996893c022afb98a17df5225e0d9d359414621
saasbook <saasbook@saasbook.(none)>
2012-08-09 07:46:31 -0700

    first part working

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,13 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  Movie.all.each do |movie|
+    puts movie.title
+  end
 end
 
 # Make sure that one string (regexp) occurs before or after another one
