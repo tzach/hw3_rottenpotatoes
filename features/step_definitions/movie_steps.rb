b0c2f5efdf7983dbd82aaf72aeffa8a3a0356e44
saasbook <saasbook@saasbook.(none)>
2012-08-09 07:56:01 -0700

    first part working (again)

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -6,9 +6,9 @@ Given /the following movies exist/ do |movies_table|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  Movie.all.each do |movie|
-    puts movie.title
-  end
+  #Movie.all.each do |movie|
+  #  puts movie.title
+  #end
 end
 
 # Make sure that one string (regexp) occurs before or after another one
